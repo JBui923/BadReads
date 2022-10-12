862a19c068ef07cd6c568c9314972dc548ecccba
Johnny Bui <johnny@Johnnys-MBP.home>
2022-10-12 03:16:19 -0700

    remove wtf validator bug

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, Validators
+from wtforms import StringField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
 
